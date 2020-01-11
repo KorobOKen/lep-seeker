be43bb57636315a3c78472b3d15a65539fb5f0c7
Dmitriy O. Korobkov <korob_ok.exe@mail.ru>
2020-01-12 00:23:49 +0300

    Конструктор карты особых точек

--- src/image_utils.rs
+++ src/image_utils.rs
@@ -1,8 +1,13 @@
 extern crate image;
 extern crate imageproc;
+extern crate itertools;
 
-use self::image::GrayImage;
+use image::{GrayImage, Luma};
 use std::path::Path;
+use imageproc::corners::Corner;
+use std::cmp::Ordering;
+use itertools::Itertools;
+use itertools::MinMaxResult::*;
 
 pub fn get_luma_by_path<P: AsRef<Path>>(path: P) -> Option<GrayImage> {
   match image::open(&path) {
@@ -13,4 +18,26 @@ pub fn get_luma_by_path<P: AsRef<Path>>(path: P) -> Option<GrayImage> {
     }
     Ok(image) => Some(image.to_luma())
   }
+}
+
+pub fn get_points_map(width: u32, height: u32, corners: Vec<Corner>) -> GrayImage {
+  let min_max_score = corners
+    .iter()
+    .map(|corner| corner.score)
+    .minmax_by(|a, b|
+      a.partial_cmp(b).unwrap_or(Ordering::Equal)
+    );
+
+  let white_pixels = vec![255u8; (width * height) as usize];
+  let mut map = GrayImage::from_raw(width, height, white_pixels).unwrap();
+
+  if let MinMax(min, max) = min_max_score {
+    let multiplier = 255f32 / (max - min);
+    for corner in corners.into_iter() {
+      let pixel_value = 255 - ((corner.score as f32 - min) * multiplier) as u8;
+      map.put_pixel(corner.x, corner.y, Luma([pixel_value]));
+    }
+  }
+
+  map
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 mod fs_utils;
 mod image_utils;
 
-extern crate imageproc;
-
 use fs_utils::get_sources_paths;
 use image_utils::get_luma_by_path;
 use imageproc::corners::{corners_fast9, corners_fast12};
+use crate::image_utils::get_points_map;
 
 fn main() {
   let paths = get_sources_paths();
@@ -16,6 +15,7 @@ fn main() {
       println!("Corners 9 amount: {}", corners9.len());
       let corners12 = corners_fast12(&luma, 100);
       println!("Corners 12 amount: {}", corners12.len());
+      get_points_map(luma.width(), luma.height(), corners9);
       println!("__________________");
     }
   }
