dfb52593d4d2821df5dbbac8c1f5e4b367df5b01
Dmitriy O. Korobkov <korob_ok.exe@mail.ru>
2020-01-12 20:30:14 +0300

    refactoring

--- src/color_seek.rs
+++ src/color_seek.rs
@@ -7,7 +7,6 @@ use palette::rgb::Srgb;
 pub fn get_color_map(src_pic: &RgbImage) -> RgbImage {
   let width = src_pic.width();
   let height = src_pic.height();
-
   let white_pixels = vec![255u8; (width * height * 3) as usize];
   let mut canvas =
     ImageBuffer::from_vec(width, height, white_pixels)
@@ -15,6 +14,7 @@ pub fn get_color_map(src_pic: &RgbImage) -> RgbImage {
 
   for (x, y, src_rgb) in src_pic.enumerate_pixels() {
     let [r, g, b] = src_rgb.0;
+
     let mut hsv: Hsv = Srgb::new(
       r as f32 / 255f32,
       g as f32 / 255f32,
@@ -23,15 +23,20 @@ pub fn get_color_map(src_pic: &RgbImage) -> RgbImage {
       .into();
     hsv.saturation = 1f32;
     hsv.value = 1f32;
+
     let result_srgb: Srgb = hsv.into();
-    let (r, g, b) = result_srgb.into_components();
-    let color = Rgb([
-      (r * 255f32).round() as u8,
-      (g * 255f32).round() as u8,
-      (b * 255f32).round() as u8
-    ]);
-    canvas.put_pixel(x, y, color);
+
+    canvas.put_pixel(x, y, srgb_to_pixel(result_srgb));
   }
 
   canvas
+}
+
+fn srgb_to_pixel(src: Srgb) -> Rgb<u8> {
+  let (r, g, b) = src.into_components();
+  Rgb([
+    (r * 255f32).round() as u8,
+    (g * 255f32).round() as u8,
+    (b * 255f32).round() as u8
+  ])
 }
